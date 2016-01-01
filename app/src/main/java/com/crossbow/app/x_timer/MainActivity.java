c6cafd9c33ab83ec0f2188e167dbd8f487d35ef1
CuiH <viewfromabove@vip.qq.com>
2016-01-01 09:03:54 +0800

    change the tab to the bottom

--- app/src/main/java/com/crossbow/app/x_timer/MainActivity.java
+++ app/src/main/java/com/crossbow/app/x_timer/MainActivity.java
@@ -8,8 +8,6 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.provider.Settings;
-import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -22,19 +20,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.crossbow.app.x_timer.fragment.MyPagerAdapter;
-import com.crossbow.app.x_timer.service.AppUsage;
 import com.crossbow.app.x_timer.service.TickTrackerService;
 
 import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        View.OnClickListener, Toolbar.OnMenuItemClickListener, ViewPager.OnPageChangeListener {
 
     private final String TAG = "main";
 
@@ -42,34 +38,32 @@ public class MainActivity extends AppCompatActivity
 
     private ServiceConnection connection;
 
+    private ViewPager viewPager;
+
+    private RadioButton tab_home;
+    private RadioButton tab_history;
+    private RadioButton tab_what;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        // toolbar
+        initToolbar();
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
+        // navigation
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        /* my code start from here */
         // 沉浸
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
 
-        // fragment tab
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(), this));
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
-        tabLayout.setupWithViewPager(viewPager);
+        // fragment and radio group
+        initTab();
 
         // check permission
         if (!hasPermission()) {
@@ -81,7 +75,6 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onServiceConnected(ComponentName name, IBinder service) {
                 usageBinder = (TickTrackerService.UsageBinder) service;
-                //watchingList = usageBinder.getWatchingList();
             }
 
             @Override
@@ -141,29 +134,138 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    // handle navigation menu
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_main) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_cloud) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_setting) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_advice) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_about) {
+
+        } else if (id == R.id.nav_check) {
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    // handle button
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.radio_home:
+                viewPager.setCurrentItem(0);
+                break;
+            case R.id.radio_history:
+                viewPager.setCurrentItem(1);
+                break;
+            case R.id.radio_what:
+                viewPager.setCurrentItem(2);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    // handle view change
+    @Override
+    public void onPageSelected(int id){
+        switch(id){
+            case 0:
+                tab_home.setChecked(true);
+                tab_home.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.home_active, 0, 0);
+
+                tab_history.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.history, 0, 0);
+                tab_what.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.what, 0, 0);
+                break;
+            case 1:
+                tab_history.setChecked(true);
+                tab_history.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.history_active, 0, 0);
+
+                tab_what.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.what, 0, 0);
+                tab_home.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.home, 0, 0);
+                break;
+            case 2:
+                tab_what.setChecked(true);
+                tab_what.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.what_active, 0, 0);
+
+                tab_home.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.home, 0, 0);
+                tab_history.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.history, 0, 0);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    // not used
+    @Override
+    public void onPageScrolled(int arg0,float arg1,int arg2) { }
+
+    // not used
+    @Override
+    public void onPageScrollStateChanged(int arg0) { }
+
+    // handle toolbar menu
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.action_share:
+                Toast.makeText(this, "click", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+    // init the toolbar
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        toolbar.setOnMenuItemClickListener(this);
+    }
+
+    // init the tab
+    private void initTab() {
+        // radio group
+        tab_home = (RadioButton) findViewById(R.id.radio_home);
+        tab_history = (RadioButton) findViewById(R.id.radio_history);
+        tab_what = (RadioButton) findViewById(R.id.radio_what);
+        tab_home.setOnClickListener(this);
+        tab_history.setOnClickListener(this);
+        tab_what.setOnClickListener(this);
+
+        // fragment
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(), this));
+
+        viewPager.addOnPageChangeListener(this);
     }
 
     // check if the user has system permission
